51b3a2ce102f222213b6c8576bb77f3be7f86c1f
Tejas59 <57625719+Tejas59@users.noreply.github.com>
2024-04-23 14:51:30 +0530

    changed cors origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://unrivaled-sable-89725f.netlify.app"],
     methods:["GET", "post"],
     credentials: true
 }));
